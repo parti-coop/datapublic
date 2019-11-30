49ebc3c62c6bbecae259e7d66096f98061796cca
Ohyeon <rest515@gmail.com>
2019-11-30 11:09:37 +0900

    Add devise for user management

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Datapublic
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Asia/Seoul'
+    config.i18n.default_locale = :ko
   end
 end
