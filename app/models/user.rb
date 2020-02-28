a0c0b0f4588ebb33ce12b9b5bb3f387de9f45253
Ohyeon <rest515@gmail.com>
2020-02-28 10:20:45 +0900

    admin can delete data set.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable, :trackable
+
+  enum role: { admin: "admin", general: "general" }
 end
