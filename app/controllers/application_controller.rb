cfacfad87b9fb9703ee15c0204a3e2e64b05c518
Ohyeon <rest515@gmail.com>
2020-03-20 21:42:43 +0900

    add basic i18n support logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = current_user.try(:locale) || extract_locale_from_accept_language_header || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
