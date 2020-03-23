75fc52bb2b253a34966ee917767e038a4655c56b
Ohyeon <rest515@gmail.com>
2020-03-23 15:42:23 +0900

    fix i18n logic when accept language is not in kr and en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def switch_locale(&action)
-    locale = current_user.try(:locale) || extract_locale_from_accept_language_header || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
-
-  private
-
-  def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    I18n.with_locale(http_accept_language.compatible_language_from(I18n.available_locales), &action)
   end
 end
