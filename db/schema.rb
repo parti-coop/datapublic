6b858f8d13b86326713521c354cfbdbd6b400520
rest515 <rest515@gmail.com>
2020-05-07 10:12:57 +0900

    메뉴명을 변경합니다

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_17_042529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_data_sets", force: :cascade do |t|
-    t.bigint "category_id"
-    t.bigint "data_set_id"
-    t.index ["category_id"], name: "index_categories_data_sets_on_category_id"
-    t.index ["data_set_id"], name: "index_categories_data_sets_on_data_set_id"
-  end
-
   create_table "data_sets", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -48,16 +34,6 @@ ActiveRecord::Schema.define(version: 2020_04_17_042529) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["data_set_id"], name: "index_links_on_data_set_id"
-  end
-
-  create_table "relateds", force: :cascade do |t|
-    t.bigint "data_set_id"
-    t.string "title"
-    t.string "url"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["data_set_id"], name: "index_relateds_on_data_set_id"
   end
 
   create_table "rumors", force: :cascade do |t|
