5f454776059284d7c445ac50599db1056ee77e94
Ohyeon <rest515@gmail.com>
2020-11-23 10:03:30 +0900

    데이터퍼블릭의 운영자 권한을 관리합니다

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,5 +4,13 @@ class Admin::UsersController < AdminController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
@@ -69,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:archive_id, :title, :cover, :body).merge(user: current_user)
+      params.require(:post).permit(:archive_id, :title, :cover, :body)
     end
 end
